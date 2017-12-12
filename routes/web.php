2868b6128f6e5f472f0edd3c3648902487e74056
textSolver34761 <pradonbenjamin@gmail.com>
2017-12-12 20:08:57 +0100

    first dev

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/form','PostController@create')->name('form');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
